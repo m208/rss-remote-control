23629f1dd25aa5e01ae9cec91de71a33796d557e
m208 <avcvet@yandex.ru>
2023-01-12 23:43:46 +0300

    feat: add WS server

--- index.ts
+++ index.ts
@@ -1,7 +1,21 @@
 import { httpServer } from "./src/http_server/index";
 import { mouse } from "@nut-tree/nut-js";
 
+import { WebSocketServer } from 'ws';
+
 const HTTP_PORT = 8181;
+const WS_PORT = 8080;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+export const wsServer = new WebSocketServer({ port: WS_PORT });
+
+wsServer.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+    ws.send(data.toString());
+  });
+
+  ws.send('something');
+});
