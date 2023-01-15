3a15cff4fdf879a0474a1398a5952392f0831193
m208 <avcvet@yandex.ru>
2023-01-16 00:42:01 +0300

    refactor: transfer data sith Streams API

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { httpServer } from "./src/http_server/index";
-
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, createWebSocketStream } from 'ws';
 import { runCommand } from "./src/ws_server/cli";
+import { readFile } from 'node:fs/promises';
 
 const HTTP_PORT = 8181;
 const WS_PORT = 8080;
@@ -13,10 +13,23 @@ httpServer.listen(HTTP_PORT);
 export const wsServer = new WebSocketServer({ port: WS_PORT });
 
 wsServer.on('connection', function connection(ws) {
-  ws.on('message', async function message(data) {
+
+  const duplex = createWebSocketStream(ws, { decodeStrings: false });
+  duplex.on('data', async (data) => {
     console.log('received: %s', data);
     const responce = await runCommand(data.toString());
-    ws.send(`${data.toString().split(' ')[0]} ${responce}`);
+    let msg = responce? responce.data : '';
+
+    if (responce && responce.type === 'file') {
+      try {
+        msg = await readFile(responce.data, {encoding: 'base64'});
+      }
+      catch { }
+    } 
+    duplex.write(`${data.toString().split(' ')[0]} ${msg}`);
+  });
+  duplex.on('close', () => { 
+    console.log("the duplex channel has closed")
   });
 }); 
 
