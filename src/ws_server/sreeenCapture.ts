f4061211ac843cadbe1b421db1a59fb49b396ff3
m208 <avcvet@yandex.ru>
2023-02-05 13:11:48 +0300

    fix: error handling on screen capturing

--- src/ws_server/sreeenCapture.ts
+++ src/ws_server/sreeenCapture.ts
@@ -3,11 +3,17 @@ import Jimp from 'jimp';
 
 export const makeScreenShot = async () => {
     const cursorPos = await mouse.getPosition();
-    const img = await screen.grabRegion(new Region(cursorPos.x, cursorPos.y, 200, 200 ));
 
-    const imgData = await new Jimp(await img.toRGB()).getBase64Async(Jimp.MIME_PNG);    
+    try {
+        const img = await screen.grabRegion(new Region(cursorPos.x, cursorPos.y, 200, 200 ));
+        const imgData = await new Jimp(await img.toRGB()).getBase64Async(Jimp.MIME_PNG);    
+        return imgData.replace('data:image/png;base64,', '');
+    }
+    catch {
+        console.log('Error: Capturing region outside of screen');
+        return '';
+    }
 
-    return imgData.replace('data:image/png;base64,', '');
 } 
 
 
